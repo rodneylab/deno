f6a7835f206462202f2c46a1a0eb86f6ebcf5e8d
Rodney Lab <codesign@rodneylab.com>
2023-01-17 19:09:14 +0000

    test(demos/deno-rust-wasm): ☑️  update unit tests

--- demos/deno-rust-wasm/rs_lib/src/lib.rs
+++ demos/deno-rust-wasm/rs_lib/src/lib.rs
@@ -19,7 +19,7 @@ macro_rules! console_log {
 const PLACEHOLDER_WIDTH: u32 = 10;
 
 #[wasm_bindgen]
-pub fn base64_placeholder(data: &mut [u8]) -> String {
+pub fn base64_placeholder(data: &[u8]) -> String {
   let image = match open_image_from_bytes(data) {
     Ok(value) => value,
     Err(_) => {
@@ -40,14 +40,24 @@ pub fn base64_placeholder(data: &mut [u8]) -> String {
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  use photon_rs::native::open_image;
+  use super::base64_placeholder;
+  use std::{fs::File, io::Read, path::Path};
 
   #[test]
-  fn it_works() {
-    let image = open_image("../content/dinosaur-lemon.jpg").unwrap();
-    assert_eq!(512, image.get_height());
-    //let result = base64_placeholder("1");
-    //assert_eq!(result, String::from("Hello"));
+  fn test_base64_placeholder() {
+    // prepare
+    let path = Path::new("../content/dinosaur-lemon.png");
+    let mut file = File::open(&path).expect("Error opening file for test");
+    let mut bytes = Vec::new();
+    file
+      .read_to_end(&mut bytes)
+      .expect("Error reading bytes into vector");
+
+    // act
+    let result = base64_placeholder(bytes.as_ref());
+
+    // assert
+    let expected = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAABfklEQVR4nA2QS0hUARiFv/869zXONE3OODTMkJVgbS1aRdAqqk1R0c5lu6AgKAhaRAthFhbVpkV7o32mu3Ez4KAgig/EJ6JXUcfxjo979f7e/Tkf53zytVJXESGKlGynw9WbWZJpm6kxD2/1gPL1DIVyGhmojGl0BhoHLUsQQ1AxMNrATSYwrTYauyfIt4G65gtJUhddFlYCjkPBDlq4jpK6YGPERb8RID8qNTVtG8Im9uI4rr+Ld+0+p1YOCQ8JY7Q4JvJlcFn9IM3toX7uVCvUel+y+fAVbrxrL7zMVqOKVXKQJ++G9Pn0b+7O/KfW3cPfR+9ZOSuTbR+l98oaM+tlNva7kLV8SounPvOZTl48/keu4xIT6zkG+57x4N4wn36+5fv0Z+TXh6SWRgTPNJl8fQPf6mHZu0W3LpFv7vDHe0PDiIn9ddSYMzhKKYWSkgjiX9uwJ1n2zSJGZNNslZCPfXlNdIWYTkSUUFrVDP6sg5tr0V70Yz0RHU8POQcy448ICLEvggAAAABJRU5ErkJggg==";
+    assert_eq!(expected, result);
   }
 }
