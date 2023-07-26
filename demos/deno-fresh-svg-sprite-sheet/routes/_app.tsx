9532487ddd19c3a786f61a50214b4e715727287c
Rodney Lab <codesign@rodneylab.com>
2023-07-26 09:32:05 +0100

    fix(demos/deno-fresh-svg-sprite-sheet): 💫 update dependencies

---
+++ demos/deno-fresh-svg-sprite-sheet/routes/_app.tsx
@@ -0,0 +1,18 @@
+import { asset, Head } from "$fresh/runtime.ts";
+import { AppProps } from "$fresh/server.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <>
+      <Head>
+			<link rel="stylesheet" href={asset("/fonts.css")} />
+        <link rel="stylesheet" href={asset("/global.css")} />
+        <link rel="icon" href={asset("/favicon.ico")} sizes="any" />
+        <link rel="icon" href={asset("/logo.svg")} type="image/svg+xml" />
+        <link rel="apple-touch-icon" href={asset("/apple-touch-icon.png")} />
+        <link rel="manifest" href={asset("/manifest.webmanifest")} />
+      </Head>
+      <Component />
+    </>
+  );
+}
--- demos/deno-fresh-svg-sprite-sheet/routes/index.tsx
+++ demos/deno-fresh-svg-sprite-sheet/routes/index.tsx
@@ -1,4 +1,4 @@
-import { asset, Head } from "$fresh/runtime.ts";
+import { Head } from "$fresh/runtime.ts";
 import BannerImage from "@/components/BannerImage.tsx";
 import { DenoIcon } from "@/components/Icons/Deno.tsx";
 import { LemonIcon } from "@/components/Icons/Lemon.tsx";
@@ -8,12 +8,6 @@ export default function Home() {
   return (
     <Fragment>
       <Head>
-        <link rel="stylesheet" href={asset("/fonts.css")} />
-        <link rel="stylesheet" href={asset("/global.css")} />
-        <link rel="icon" href={asset("/favicon.ico")} sizes="any" />
-        <link rel="icon" href={asset("/logo.svg")} type="image/svg+xml" />
-        <link rel="apple-touch-icon" href={asset("/apple-touch-icon.png")} />
-        <link rel="manifest" href={asset("/manifest.webmanifest")} />
         <link rel="preconnect" href="www.youtube-nocookie.com" />
         <link rel="dns-prefetch" href="www.youtube-nocookie.com" />
         <title>Trying Deno Fresh 🍋</title>
