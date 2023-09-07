979570b2f12f3dd1a8ffe9ca4884366faafea182
Rodney Lab <codesign@rodneylab.com>
2023-09-07 06:34:44 +0100

    fix(demos/trying-deno-fresh): 💫 update dependencies

---
+++ demos/trying-deno-fresh/routes/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from "$fresh/server.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>my-fresh-project</title>
+        <link rel="stylesheet" href="/fonts.css" />
+        <link rel="stylesheet" href="/global.css" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </head>
+      <body>
+        <Component />
+      </body>
+    </html>
+  );
+}
