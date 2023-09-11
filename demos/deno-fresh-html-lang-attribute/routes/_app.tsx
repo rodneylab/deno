8b43b55389c15a0339ff40d3dde2f009d9f1e0b9
Rodney Lab <codesign@rodneylab.com>
2023-09-11 09:40:57 +0100

    fix(demos/deno-fresh-html-lang-attribute): 💫 update dependencies

--- demos/deno-fresh-html-lang-attribute/routes/_app.tsx
+++ demos/deno-fresh-html-lang-attribute/routes/_app.tsx
@@ -1,8 +1,25 @@
+import { asset } from "$fresh/runtime.ts";
 import type { AppProps } from "$fresh/server.ts";
 
 export default function App({ Component }: AppProps) {
   return (
     <html lang="en-GB">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>my-fresh-project</title>
+        <link rel="stylesheet" href="/styles.css" />
+        <link rel="icon" href={asset("/favicon.ico")} sizes="any" />
+        <link rel="icon" href={asset("/icon.svg")} type="image/svg+xml" />
+        <link rel="apple-touch-icon" href={asset("/apple-touch-icon.png")} />
+        <link rel="manifest" href={asset("/manifest.webmanifest")} />
+        <link
+          rel="alternate"
+          title="Rodney Lab Camera Blog"
+          href="/rss.xml"
+          type="application/rss+xml"
+        />
+      </head>
       <body>
         <Component />
       </body>
--- demos/deno-fresh-html-lang-attribute/routes/index.tsx
+++ demos/deno-fresh-html-lang-attribute/routes/index.tsx
@@ -1,4 +1,4 @@
-import { asset, Head } from "$fresh/runtime.ts";
+import { Head } from "$fresh/runtime.ts";
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
@@ -6,16 +6,6 @@ export default function Home() {
     <>
       <Head>
         <title>Fresh App</title>
-        <link rel="icon" href={asset("/favicon.ico")} sizes="any" />
-        <link rel="icon" href={asset("/icon.svg")} type="image/svg+xml" />
-        <link rel="apple-touch-icon" href={asset("/apple-touch-icon.png")} />
-        <link rel="manifest" href={asset("/manifest.webmanifest")} />
-        <link
-          rel="alternate"
-          title="Rodney Lab Camera Blog"
-          href="/rss.xml"
-          type="application/rss+xml"
-        />
       </Head>
       <div>
         <img
