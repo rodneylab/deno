0c496725f81bc83b259996872e27fd17f7c5dea2
Rodney Lab <codesign@rodneylab.com>
2023-09-19 08:59:25 +0100

    fix(demos/deno-fresh-rss-feed): 💫 update dependencies

---
+++ demos/deno-fresh-rss-feed/routes/_app.tsx
@@ -0,0 +1,19 @@
+import { asset } from "$fresh/runtime.ts";
+import { AppProps } from "$fresh/server.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <html>
+      <head>
+        <link rel="icon" href={asset("/favicon.ico")} sizes="any" />
+        <link rel="icon" href={asset("/icon.svg")} type="image/svg+xml" />
+        <link rel="apple-touch-icon" href={asset("/apple-touch-icon.png")} />
+        <link rel="manifest" href={asset("/manifest.webmanifest")} />
+        <meta name="viewport" content="width=device-width" />
+      </head>
+      <body>
+        <Component />
+      </body>
+    </html>
+  );
+}
--- demos/deno-fresh-rss-feed/routes/index.tsx
+++ demos/deno-fresh-rss-feed/routes/index.tsx
@@ -1,15 +1,11 @@
 import { Head } from "$fresh/runtime.ts";
-import { asset } from "$fresh/runtime.ts";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Rodney Lab Camera Blog</title>
-        <link rel="icon" href={asset("/favicon.ico")} sizes="any" />
-        <link rel="icon" href={asset("/icon.svg")} type="image/svg+xml" />
-        <link rel="apple-touch-icon" href={asset("/apple-touch-icon.png")} />
-        <link rel="manifest" href={asset("/manifest.webmanifest")} />
+
         <link
           rel="alternate"
           title="Rodney Lab Camera Blog"
