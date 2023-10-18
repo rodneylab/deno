1d89ce35a480895d3b05a2050d6986a25f0745a6
Rodney Lab <codesign@rodneylab.com>
2023-10-18 16:34:27 +0100

    fix(demos/deno-fresh-svg-sprite-sheet): 💫 update dependencies

--- demos/deno-fresh-svg-sprite-sheet/routes/_app.tsx
+++ demos/deno-fresh-svg-sprite-sheet/routes/_app.tsx
@@ -1,10 +1,10 @@
-import { asset, Head } from "$fresh/runtime.ts";
+import { asset } from "$fresh/runtime.ts";
 import { AppProps } from "$fresh/server.ts";
 
 export default function App({ Component }: AppProps) {
   return (
-    <>
-      <Head>
+    <html lang="en-GB">
+      <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width" />
         <link rel="stylesheet" href={asset("/fonts.css")} />
@@ -13,8 +13,10 @@ export default function App({ Component }: AppProps) {
         <link rel="icon" href={asset("/logo.svg")} type="image/svg+xml" />
         <link rel="apple-touch-icon" href={asset("/apple-touch-icon.png")} />
         <link rel="manifest" href={asset("/manifest.webmanifest")} />
-      </Head>
-      <Component />
-    </>
+      </head>
+      <body>
+        <Component />
+      </body>
+    </html>
   );
 }
