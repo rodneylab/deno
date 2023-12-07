a94435cd18d1d2c443310da521608396dcf41d56
Rodney Lab <codesign@rodneylab.com>
2023-12-07 07:45:01 +0000

    fix(demos/deno-fresh-svg-sprite-sheet): 💫 update dependencies

--- demos/deno-fresh-svg-sprite-sheet/fresh.gen.ts
+++ demos/deno-fresh-svg-sprite-sheet/fresh.gen.ts
@@ -2,19 +2,20 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/_app.tsx";
-import * as $1 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $_app from "./routes/_app.tsx";
+import * as $index from "./routes/index.tsx";
+import * as $Counter from "./islands/Counter.tsx";
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/_app.tsx": $0,
-    "./routes/index.tsx": $1,
+    "./routes/_app.tsx": $_app,
+    "./routes/index.tsx": $index,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
+    "./islands/Counter.tsx": $Counter,
   },
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
