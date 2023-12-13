5742015d50cad82f3302b4ea239e31f18dbd36ef
Rodney Lab <codesign@rodneylab.com>
2023-12-13 05:48:00 +0000

    fix(demos/deno-fresh-testing): 💫 update dependencies

--- demos/deno-fresh-testing/fresh.gen.ts
+++ demos/deno-fresh-testing/fresh.gen.ts
@@ -2,25 +2,26 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
-import * as $$1 from "./islands/Video.tsx";
+import * as $_name_ from "./routes/[name].tsx";
+import * as $_app from "./routes/_app.tsx";
+import * as $api_joke from "./routes/api/joke.ts";
+import * as $index from "./routes/index.tsx";
+import * as $Counter from "./islands/Counter.tsx";
+import * as $Video from "./islands/Video.tsx";
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/_app.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/[name].tsx": $_name_,
+    "./routes/_app.tsx": $_app,
+    "./routes/api/joke.ts": $api_joke,
+    "./routes/index.tsx": $index,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
-    "./islands/Video.tsx": $$1,
+    "./islands/Counter.tsx": $Counter,
+    "./islands/Video.tsx": $Video,
   },
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
