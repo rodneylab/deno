a66e73fb9e76e0b2894db0f4a743d31ac8cdecdb
Rodney Lab <codesign@rodneylab.com>
2024-06-07 16:27:00 +0100

    build(demos/trying-deno-fresh): 🧱 update dependencies

--- demos/trying-deno-fresh/main.ts
+++ demos/trying-deno-fresh/main.ts
@@ -4,10 +4,12 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import "$std/dotenv/load.ts";
+import { load } from "@std/dotenv";
+
+await load({ export: true });
 
 import { start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
-import config from "./fresh.config.ts";
+import manifest from "@/fresh.gen.ts";
+import config from "@/fresh.config.ts";
 
 await start(manifest, config);
