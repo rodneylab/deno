931da344b13b71358235aa74f711ef69f99cc087
Rodney Lab <codesign@rodneylab.com>
2024-06-19 06:19:35 +0100

    build(demos/deno-fresh-svg-sprite-sheet): 🧱 update dependencies

--- demos/deno-fresh-svg-sprite-sheet/main.ts
+++ demos/deno-fresh-svg-sprite-sheet/main.ts
@@ -4,9 +4,11 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import "$std/dotenv/load.ts";
+import { load } from "@std/dotenv";
+
+await load({ export: true });
 
 import { start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
+import manifest from "@/fresh.gen.ts";
 
 await start(manifest);
