75c46a3cd7743bc12f66c264618f20b6abbd9cb8
Rodney Lab <codesign@rodneylab.com>
2024-08-21 13:02:05 +0100

    build(demos/den-fresh-stylelint): 🧱 update dependencies

--- demos/deno-fresh-stylelint/fresh.gen.ts
+++ demos/deno-fresh-stylelint/fresh.gen.ts
@@ -6,7 +6,7 @@ import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $index from "./routes/index.tsx";
 import * as $Video from "./islands/Video.tsx";
-import { type Manifest } from "$fresh/server.ts";
+import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
