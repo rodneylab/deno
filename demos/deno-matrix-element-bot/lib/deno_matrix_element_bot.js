fa7cfb9435471b9725517e3cd3bbe1c8c20366ab
Rodney Lab <codesign@rodneylab.com>
2025-03-06 08:50:35 +0000

    build(demos/deno-matrix-element-bot): 🧱 update dependencies

--- demos/deno-matrix-element-bot/lib/deno_matrix_element_bot.js
+++ demos/deno-matrix-element-bot/lib/deno_matrix_element_bot.js
@@ -4,8 +4,9 @@
 // deno-fmt-ignore-file
 // @ts-self-types="./deno_matrix_element_bot.d.ts"
 
-// source-hash: c060e678ec95c2af5da55a6d9877d938baa1213f
+// source-hash: ef26e7461ede107f3a08d6f572d4923cf0002e76
 import * as wasm from "./deno_matrix_element_bot.wasm";
 export * from "./deno_matrix_element_bot.internal.js";
 import { __wbg_set_wasm } from "./deno_matrix_element_bot.internal.js";
 __wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
